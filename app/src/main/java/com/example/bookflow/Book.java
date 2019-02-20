9549501cac547dc6fc8d0bfbfd6211fb95545a20
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2019-02-20 14:37:03 -0700

    Update Book.java

--- app/src/main/java/com/example/bookflow/Book.java
+++ app/src/main/java/com/example/bookflow/Book.java
@@ -3,12 +3,18 @@ package com.example.bookflow;
 class Book {
     String title;
     String author;
-    int ISBN;
-    Book(String title, String author, int ISBN){
+    
+    public Book(String title, String author){
         this.title = title;
         this.author = author;
-        this.ISBN = ISBN;
     }
-
+    
+    public getTitle(){
+        return this.title;
+    }
+    
+    public getAuthor(){
+        return this.author;
+    }
 
 }
