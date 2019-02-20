0e6a0de66c5c3978a2c89c012a707cf6de932448
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2019-02-20 16:22:00 -0700

    Update Book.java

--- app/src/main/java/com/example/bookflow/Book.java
+++ app/src/main/java/com/example/bookflow/Book.java
@@ -15,6 +15,8 @@ class Book {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.rating = 0;
+        this.requestCount = 0;
     }
     
     public String getTitle(){
@@ -37,4 +39,49 @@ class Book {
         return this.rating;
     }
     
+        public void setStatus(String newStatus){
+        this.status = newStatus;
+    }
+    
+    public void addPhoto(BookPhoto photo){
+        photos.add(photo);
+    }
+    
+    public Photo getPhoto(int index){
+        return photos.get(index);
+    }
+    
+    public void deletePhoto(int index){
+        photos.remove(index);
+    }
+    
+    public User getOwner(){
+        return this.owner;
+    }
+    
+    public void setOwner(User owner){
+        this.owner = owner;
+    }
+    
+    public User getBorrower(){
+        return this.borrower;
+    }
+    
+    public void setBorrower(User newBorrower){
+        this.borrower = newBorrower;
+    }
+    
+    public void countIncrease(){
+        this.requestCount ++;
+    }
+    
+    public int getCount(){
+        return this.requestCount;
+    }
+    
+    public void setDescription(String isbn, String title, String author){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
 }
