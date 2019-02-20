c23ba90fccc6e864e124339e39c0144923b017ef
sluxsr <roy996812@gmail.com>
2019-02-20 14:35:13 -0700

    Review class added.

--- app/src/main/java/com/example/bookflow/Review.java
+++ app/src/main/java/com/example/bookflow/Review.java
@@ -1,4 +1,24 @@
 package com.example.bookflow;
 
 class Review {
+    private User reviewer;
+    private User reviewee;
+    private String comments;
+    private Integer rating;
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public User getReviewee() {
+        return reviewee;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
 }
