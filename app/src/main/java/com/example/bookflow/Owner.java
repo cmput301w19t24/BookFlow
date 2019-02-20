cedf8e05d0a2b9925822fe02cec253a769152c2a
Yuhan Ye <2232191103@qq.com>
2019-02-20 15:27:38 -0700

    add function in owner interface

--- app/src/main/java/com/example/bookflow/Owner.java
+++ app/src/main/java/com/example/bookflow/Owner.java
@@ -1,15 +1,32 @@
 package com.example.bookflow;
 
 public interface Owner {
-    void addBook(BookCopy bookCopy);
-    void getBookDescription(int ISBN);
-    void setStatus(BookCopy bookCopy);
-    void viewOwnedBooks();
-    void editBookDescription();
-    void deleteBook();
-    void viewRequest(Book book);
-    void acceptRequest(Book book);
-    void declineRequest(Book book);
+    public void addBook(BookCopy bookCopy);
 
+    public void getBookDescription(String ISBN);
+
+    public void setStatus(BookCopy bookCopy);
+
+    public void viewOwnedBooks();
+
+    public void editBookDescription();
+
+    public void deleteBook();
+
+    public void viewRequest(BookCopy bookCopy);
+
+    public void acceptRequest(BookCopy bookCopy);
+
+    public void declineRequest(BookCopy bookCopy);
+
+    public void handOverBook(BookCopy bookCopy, String ISBN);
+
+    public void receiveReturnedBook(BookCopy bookCopy, String ISBN);
+
+    public void attachPhoto(BookCopy bookCopy);
+
+    public void deletePhoto(BookCopy bookCopy);
+
+    public void specifyGeolocation();
 }
 
