2013a5fc9796d755a1d5285809fd7dd5c7e3c4fb
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2019-02-20 14:48:22 -0700

    Update BookCopy.java

--- app/src/main/java/com/example/bookflow/BookCopy.java
+++ app/src/main/java/com/example/bookflow/BookCopy.java
@@ -1,4 +1,46 @@
 package com.example.bookflow;
 
+import java.util.ArrayList;
+
 public class BookCopy{
+    Book book;
+    int isbn;
+    String status;
+    User owner;
+    User borrower;
+    ArrayList<Photo> photos = new ArrayList<>();
+    
+    public BookCopy(Book book, int isbn, User owner){
+        this.book = book;
+        this.isbn = isbn;
+        this.owner = owner;
+    }
+    
+    public setStatus(String newStatus){
+        this.status = newStatus;
+    }
+    
+    public addPhoto(BookPhoto photo){
+        photos.add(photo);
+    }
+    
+    public getPhoto(int index){
+        return photos.get(index);
+    }
+    
+    public deletePhoto(int index){
+        photos.remove(index);
+    }
+    
+    public getOwner(){
+        return this.owner;
+    }
+    
+    public getBorrower(){
+        return this.borrower;
+    }
+    
+    public setBorrower(User newborrower){
+        this.borrower = newborrower;
+    }
 }
