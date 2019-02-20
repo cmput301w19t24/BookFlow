9b4af700a7fc2700fe518c41080c839670197c4e
sluxsr <roy996812@gmail.com>
2019-02-20 15:06:10 -0700

    Owner modified.

--- app/src/main/java/com/example/bookflow/Owner.java
+++ app/src/main/java/com/example/bookflow/Owner.java
@@ -1,15 +1,15 @@
 package com.example.bookflow;
 
 public interface Owner {
-    public void addBook(BookCopy bookCopy);
-    public void getBookDescription(int ISBN);
-    public void setStatus(BookCopy bookCopy);
-    public void viewOwnedBooks();
-    public void editBookDescription();
-    public void deleteBook();
-    public void viewRequest(Book book);
-    public void acceptRequest(Book book);
-    public void declineRequest(Book book);
+    void addBook(BookCopy bookCopy);
+    void getBookDescription(int ISBN);
+    void setStatus(BookCopy bookCopy);
+    void viewOwnedBooks();
+    void editBookDescription();
+    void deleteBook();
+    void viewRequest(Book book);
+    void acceptRequest(Book book);
+    void declineRequest(Book book);
 
 }
 
