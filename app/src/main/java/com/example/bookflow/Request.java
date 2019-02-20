0db2d8bb0e9bf7cebfd213ad301adbb565fa966a
Paul Liu <puer@ualberta.ca>
2019-02-20 14:43:40 -0700

    add request class

--- app/src/main/java/com/example/bookflow/Request.java
+++ app/src/main/java/com/example/bookflow/Request.java
@@ -1,4 +1,46 @@
 package com.example.bookflow;
 
-class Request {
+/**
+ * This class models the relationship between two users with book
+ */
+public class Request {
+    public Borrower getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public BookCopy getBook() {
+        return book;
+    }
+
+    public void setBook(BookCopy book) {
+        this.book = book;
+    }
+
+    /**
+     * The borrower-side of the request
+     */
+    private Borrower borrower;
+
+    /**
+     * The owner-side of the request
+     */
+    private Owner owner;
+
+    /**
+     * The book
+     */
+    private BookCopy book;
+
 }
