c65331aab93d3c446fcb130ceffad460fc721842
Yuhan Ye <2232191103@qq.com>
2019-02-20 18:15:58 -0700

    book unit test photo url

--- app/src/test/java/com/example/bookflow/BookUnitTest.java
+++ app/src/test/java/com/example/bookflow/BookUnitTest.java
@@ -2,6 +2,9 @@ package com.example.bookflow;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class BookUnitTest {
@@ -16,7 +19,13 @@ public class BookUnitTest {
     
     @Test
     public void testAddAndGetPhoto() {
-        BookPhoto photo1 = new BookPhoto("https://en.wikipedia.org/wiki/Java_(programming_language)#/media/File:Java_programming_language_logo.svg");
+        URL url = null;
+        try {
+            url = new URL("https://en.wikipedia.org/wiki/Java_(programming_language)#/media/File:Java_programming_language_logo.svg");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        BookPhoto photo1 = new BookPhoto(url);
         book1.addPhoto(photo1);
         assertEquals(photo1, book1.getPhoto(0));
     }
@@ -24,6 +33,6 @@ public class BookUnitTest {
     @Test
     public void testDeletePhoto() {
         book1.deletePhoto(0);
-        assertEquals(0, book1.getAllPhotos().size());
+        assertEquals(0, book1.getAllPhoto().size());
     }
 }
