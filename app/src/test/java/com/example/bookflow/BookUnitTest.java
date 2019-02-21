3bdf2722e34bae9e8974a2fda6bc50f24f27b105
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2019-02-20 17:49:19 -0700

    Update BookUnitTest.java

--- app/src/test/java/com/example/bookflow/BookUnitTest.java
+++ app/src/test/java/com/example/bookflow/BookUnitTest.java
@@ -16,7 +16,7 @@ public class BookUnitTest {
     
     @Test
     public void testAddAndGetPhoto() {
-        Photo photo1 = new Photo();
+        Photo photo1 = new Photo("https://en.wikipedia.org/wiki/Java_(programming_language)#/media/File:Java_programming_language_logo.svg");
         book1.addPhoto(photo1);
         assertEqual(photo1, book1.getPhoto(0));
     }
