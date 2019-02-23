7f76c1c155dd4c51e45001b9a4b198a26f959abe
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2019-02-22 22:06:32 -0700

    Update BookUnitTest.java

--- app/src/test/java/com/example/bookflow/BookUnitTest.java
+++ app/src/test/java/com/example/bookflow/BookUnitTest.java
@@ -35,4 +35,56 @@ public class BookUnitTest {
         book1.deletePhoto(0);
         assertEquals(0, book1.getAllPhoto().size());
     }
+    
+    @Test
+    public void testSetTitle(){
+        book1.setTitle("My Story");
+        assertEquals("My Story", book1.getTitle());
+    }
+    
+    @Test
+    public void testSetAuthor(){
+        book1.setAuthor("Jinming");
+        assertEquals("Jinming", book1.getAuthor());
+    }
+    
+    @Test
+    public void testSetIsbn(){
+        book1.setIsbn("0123456789");
+        assertEquals("0123456789", book1.getIsbn());
+    }
+    
+    @Test
+    public void testSetRating(){
+        book1.setRating(5);
+        assertEquals(5, book1.getRating());
+    }
+    
+    @Test
+    public void testSetStatus(){
+        book1.setStatus("Available");
+        assertEquals("Available", book1.getStatus());
+    }
+    
+    @Test
+    public void testSetOwner(){
+        User user1 = new User();
+        book1.setOwner(user1);
+        assertEquals(user1, book1.getOwner());
+    }
+    
+    @Test
+    public void testSetBorrower(){
+        User user1 = new User();
+        book1.setStatus(user1);
+        assertEquals(user1, book1.getBorrower());
+    }
+    
+    @Test
+    public void testSetDescription(){
+        book1.setDescription("9876543210", "My New Story", "New Jinming");
+        assertEquals("9876543210", book1.getIsbn());
+        assertEquals("My New Story", book1.getTitle());
+        assertEquals("New Jinming", book1.getAuthor());
+    }
 }
