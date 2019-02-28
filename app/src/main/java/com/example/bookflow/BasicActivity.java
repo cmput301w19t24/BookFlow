dec4d91eb54259992c0c969a83e3f0c27d9a568a
Yuhan Ye <2232191103@qq.com>
2019-02-28 16:21:48 -0700

    add book detail page

--- app/src/main/java/com/example/bookflow/BasicActivity.java
+++ app/src/main/java/com/example/bookflow/BasicActivity.java
@@ -3,7 +3,7 @@ package com.example.bookflow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Basic extends AppCompatActivity {
+public class BasicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
