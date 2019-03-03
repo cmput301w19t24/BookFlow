358887cf45fa70193b49809a939f6454757ce555
sluxsr <roy996812@gmail.com>
2019-03-03 16:20:33 -0700

    .

--- app/src/main/java/com/example/bookflow/BookDetailActivity.java
+++ app/src/main/java/com/example/bookflow/BookDetailActivity.java
@@ -10,4 +10,5 @@ public class BookDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
     }
+
 }
--- app/src/main/java/com/example/bookflow/MainActivity.java
+++ app/src/main/java/com/example/bookflow/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.bookflow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ListView myBookList;
+    private static ListView myBorrowList;
+//    private static MyAdapter adapter;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myBookList = (ListView) findViewById(R.id.myBookList);
+        myBorrowList = (ListView) findViewById(R.id.myBorrowList);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        adapter = new MyAdapter();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!", null);
     }
 }
