669c9beeafd559e8d2262bd960dff28565058ebd
j-liew <justinwcliew@gmail.com>
2019-03-03 17:11:17 -0700

    add book, photo

---
+++ app/src/main/java/com/example/bookflow/BookPhotosActivity.java
@@ -0,0 +1,12 @@
+package com.example.bookflow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BookPhotosActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_photos);
+    }
+}
