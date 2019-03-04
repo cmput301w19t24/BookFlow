096850d5f30b96fc46f2e1bcada526934fd56e36
sluxsr <roy996812@gmail.com>
2019-03-03 17:15:58 -0700

    sign up/login/main page

--- app/src/main/java/com/example/bookflow/LoginActivity.java
+++ app/src/main/java/com/example/bookflow/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.bookflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends BasicActivity {
 
@@ -11,5 +13,13 @@ public class LoginActivity extends BasicActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void loggedIn(View loggedInView) {
+        Intent intent_loggedIn = new Intent(this, MainActivity.class);
+        startActivity(intent_loggedIn);
+    }
 
+    public void signUp(View signUpView) {
+        Intent intent_signUp = new Intent(this, SignUpActivity.class);
+        startActivity(intent_signUp);
+    }
 }
--- app/src/main/java/com/example/bookflow/SignUpActivity.java
+++ app/src/main/java/com/example/bookflow/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.bookflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends BasicActivity {
 
@@ -9,5 +11,10 @@ public class SignUpActivity extends BasicActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signedUpIn(View signedUpView) {
+        Intent intent_signedUp = new Intent(this, MainActivity.class);
+        startActivity(intent_signedUp);
     }
 }
