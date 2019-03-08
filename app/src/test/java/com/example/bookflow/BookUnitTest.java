9d5b35b5bce9ebcae861aba73691b706606821d3
Paul Liu <puer@ualberta.ca>
2019-03-07 23:54:03 -0700

    remove photo class

--- app/src/test/java/com/example/bookflow/BookUnitTest.java
+++ app/src/test/java/com/example/bookflow/BookUnitTest.java
@@ -3,14 +3,10 @@ package com.example.bookflow;
 import android.net.Uri;
 
 import com.example.bookflow.Model.Book;
-import com.example.bookflow.Model.Photo;
 import com.example.bookflow.Model.User;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class BookUnitTest {
     Book book1 = new Book("ABC", "jinming", "0123456789123");
