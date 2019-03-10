4d8ee586cc5b02bd4d3ed975e19d516dc1697957
Paul Liu <puer@ualberta.ca>
2019-03-10 01:41:33 -0700

    remove unnecessary imports in ScanActivity

--- app/src/main/java/com/example/bookflow/ScanActivity.java
+++ app/src/main/java/com/example/bookflow/ScanActivity.java
@@ -19,9 +19,6 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
