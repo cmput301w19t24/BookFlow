1545f84eb6a4283ca2317182cb6ecb33fe25c88f
sluxsr <roy996812@gmail.com>
2019-03-11 11:49:40 -0600

    Add intent test login Activity

---
+++ app/src/androidTest/java/com/example/bookflow/LoginActivityTest.java
@@ -0,0 +1,77 @@
+package com.example.bookflow;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class LoginActivityTest extends ActivityTestRule<LoginActivity> {
+
+    private Solo solo;
+    private FirebaseAuth mAuth;
+
+
+    public LoginActivityTest(){
+        super(LoginActivity.class, false, true);
+    }
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, false, true);
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), rule.getActivity());
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword("shengyao@ualberta.ca", "123456");
+    }
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+    @Test
+    public void clickLogin(){
+        LoginActivity activity = (LoginActivity) solo.getCurrentActivity();
+
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.clearEditText((EditText) solo.getView(R.id.email));
+        solo.clearEditText((EditText) solo.getView(R.id.password));
+        solo.enterText((EditText) solo.getView(R.id.email), "shengyao@ualberta.ca");
+        solo.enterText((EditText) solo.getView(R.id.password), "123456");
+        Button loginbtn = (Button)solo.getView("login");
+        solo.clickOnView(loginbtn);
+        CheckBox checkBox = (CheckBox) solo.getView(R.id.checkBox);
+        solo.clickOnView(checkBox);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+    @Test
+    public void rememberLogin(){
+        LoginActivity activity = (LoginActivity) solo.getCurrentActivity();
+
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        Button loginbtn = (Button)solo.getView("login");
+        solo.clickOnView(loginbtn);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
