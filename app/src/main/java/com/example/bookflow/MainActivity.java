8e7cb20ddc1badb8cb0aedc5e633aac8a1ee4958
sluxsr <roy996812@gmail.com>
2019-03-11 00:04:56 -0600

    modified mainactivity

--- app/src/main/java/com/example/bookflow/MainActivity.java
+++ app/src/main/java/com/example/bookflow/MainActivity.java
@@ -16,26 +16,19 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends BasicActivity {
 
     private static ListView myBookList;
-    private static ListView myBorrowList;
     private MyAdapter adpBook;
-    private MyAdapter adpBorrow;
     private FirebaseDatabase database;
     private FirebaseAuth mAuth;
     private String uid;
@@ -75,12 +68,8 @@ public class MainActivity extends BasicActivity {
         query = database.getReference().child("Books");
 
         ArrayList<Book> books = new ArrayList<>();
-        ArrayList<Book> borrows = new ArrayList<>();
         adpBook = new MyAdapter(this,books);
-        adpBorrow = new MyAdapter(this,borrows);
-
         bookList();
-        borrowList();
     }
 
     private void bookList() {
@@ -119,66 +108,7 @@ public class MainActivity extends BasicActivity {
         });
     }
 
-    private void borrowList() {
-        myBorrowList = (ListView) findViewById(R.id.myBorrowList);
-        query.orderByChild("borrowerId").equalTo(uid).addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                System.out.println("Added " + " " +dataSnapshot.getValue());
-                adpBorrow.add(dataSnapshot.getValue(Book.class));
-            }
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) { }
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) { }
-        });
-        myBorrowList.setAdapter(adpBorrow);
-        myBorrowList.setClickable(true);
-        myBorrowList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            /**
-             * @param parent
-             * @param view
-             * @param position
-             * @param id
-             */
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent_detail = new Intent(MainActivity.this, BookDetailActivity.class);
-                String book_id = adpBorrow.getItem(position).getBookId();
-                intent_detail.putExtra("book_id",book_id);
-                startActivity(intent_detail);
-            }
-        });
-    }
-
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        adapterBook.startListening();
-//
-//        // TODO: delete after testing firebase
-////        DatabaseReference myRef = database.getReference("Books");
-////        myRef.child("Author").setValue("1", null);
-//
-//
-//    }
-
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        adapterBook.stopListening();
-//    }
-
-    public void seeMoreBooks(View moreBookView) {
-        Intent intent_booklist = new Intent(this, BookListActivity.class);
-        startActivity(intent_booklist);
-    }
-
-    public void seeMoreBorrows(View moreBorrowView) {
+    public void seeBorrowList(View borrowlistV) {
         Intent intent_borrowlist = new Intent(this, BorrowListActivity.class);
         startActivity(intent_borrowlist);
     }
