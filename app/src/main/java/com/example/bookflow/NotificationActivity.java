7fb70ff711847119c18bf26ef73073568dffa7c9
sluxsr <roy996812@gmail.com>
2019-03-10 18:46:48 -0600

    .

--- app/src/main/java/com/example/bookflow/NotificationActivity.java
+++ app/src/main/java/com/example/bookflow/NotificationActivity.java
@@ -15,44 +15,44 @@ import com.google.firebase.database.FirebaseDatabase;
 
 
 public class NotificationActivity extends BasicActivity {
-    private FirebaseAuth mAuth;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    /////
-        setContentView(R.layout.activity_notification);
-
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
-        String user_id = user.getUid();
-
-        DatabaseReference notificationRef = FirebaseDatabase.getInstance().getReference().child("Notifications").child(user_id);
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-
-        recyclerView.setLayoutManager(llm);
-        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
-                llm.getOrientation());
-        recyclerView.addItemDecoration(mDividerItemDecoration);
-
-        FirebaseRecyclerAdapter<com.example.bookflow.Model.Notification, NotificationHolder> adapter = new FirebaseRecyclerAdapter<com.example.bookflow.Model.Notification, NotificationHolder>(com.example.bookflow.Model.Notification.class, R.layout.notification_list_item, NotificationHolder.class, notificationRef) {
-            @Override
-            protected void populateViewHolder(NotificationHolder viewHolder, Notification model, int position) {
-                String outString = "";
-                String sender = model.getSenderName();
-                String book = model.getBookTitle();
-                if (model.getType().equals("request")) {
-                    viewHolder.setNotificationType("Book Request");
-                    outString = sender + " has requested " + "\"" + book + "\"";
-                }
-
-                viewHolder.setNotificationText(outString);
-            }
-        };
-        recyclerView.setAdapter(adapter);
-
-    }
+//    private FirebaseAuth mAuth;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    /////
+//        setContentView(R.layout.activity_notification);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        String user_id = user.getUid();
+//
+//        DatabaseReference notificationRef = FirebaseDatabase.getInstance().getReference().child("Notifications").child(user_id);
+//
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+//
+//        LinearLayoutManager llm = new LinearLayoutManager(this);
+//
+//        recyclerView.setLayoutManager(llm);
+//        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+//                llm.getOrientation());
+//        recyclerView.addItemDecoration(mDividerItemDecoration);
+//
+//        FirebaseRecyclerAdapter<com.example.bookflow.Model.Notification, NotificationHolder> adapter = new FirebaseRecyclerAdapter<com.example.bookflow.Model.Notification, NotificationHolder>(com.example.bookflow.Model.Notification.class, R.layout.notification_list_item, NotificationHolder.class, notificationRef) {
+//            @Override
+//            protected void populateViewHolder(NotificationHolder viewHolder, Notification model, int position) {
+//                String outString = "";
+//                String sender = model.getSenderName();
+//                String book = model.getBookTitle();
+//                if (model.getType().equals("request")) {
+//                    viewHolder.setNotificationType("Book Request");
+//                    outString = sender + " has requested " + "\"" + book + "\"";
+//                }
+//
+//                viewHolder.setNotificationText(outString);
+//            }
+//        };
+//        recyclerView.setAdapter(adapter);
+//
+//    }
 }
--- app/src/main/java/com/example/bookflow/UserProfileActivity.java
+++ app/src/main/java/com/example/bookflow/UserProfileActivity.java
@@ -67,23 +67,23 @@ public class UserProfileActivity extends BasicActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        reviewListView = (ListView) findViewById(R.id.reviewList);
-        offerListView = (ListView) findViewById(R.id.offerList);
-        requestListView = (ListView) findViewById(R.id.requestList);
-        dbRef = FirebaseDatabase.getInstance().getReference();
-
-        // determine whether it's yourself visiting your profile or other user visiting your profile
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(SearchActivity.EXTRA_MESSAGE);
-        if (message != null) {
-            ImageView imageButton = findViewById(R.id.editPersonInfo);
-            imageButton.setEnabled(false);
-            imageButton.setVisibility(View.INVISIBLE);
-            uid = message;
-        } else  {
-            FirebaseUser user = mAuth.getCurrentUser();
-            uid = user.getUid();
-        }
+//        reviewListView = (ListView) findViewById(R.id.reviewList);
+//        offerListView = (ListView) findViewById(R.id.offerList);
+//        requestListView = (ListView) findViewById(R.id.requestList);
+//        dbRef = FirebaseDatabase.getInstance().getReference();
+//
+//        // determine whether it's yourself visiting your profile or other user visiting your profile
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(SearchActivity.EXTRA_MESSAGE);
+//        if (message != null) {
+//            ImageView imageButton = findViewById(R.id.editPersonInfo);
+//            imageButton.setEnabled(false);
+//            imageButton.setVisibility(View.INVISIBLE);
+//            uid = message;
+//        } else  {
+//            FirebaseUser user = mAuth.getCurrentUser();
+//            uid = user.getUid();
+//        }
 
         setUpImageView();
         setUpBasicInfo();
