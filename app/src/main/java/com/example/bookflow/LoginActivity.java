338670a12a4f6c3a416286a1aa8f6b7a2679f85e
Paul Liu <puer@ualberta.ca>
2019-03-14 17:37:18 -0600

    login page not a sub class of basic activity

--- app/src/main/java/com/example/bookflow/LoginActivity.java
+++ app/src/main/java/com/example/bookflow/LoginActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -28,7 +29,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 
-public class LoginActivity extends BasicActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private EditText email, password;
     private TextView signup;
