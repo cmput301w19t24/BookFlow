4f0a0933b0a62c22200c1532f259b9ea7f8009aa
j-liew <justinwcliew@gmail.com>
2019-03-14 12:26:34 -0600

    added view requests for book feature

---
+++ app/src/main/java/com/example/bookflow/RequestHolder.java
@@ -0,0 +1,88 @@
+package com.example.bookflow;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+public class RequestHolder extends RecyclerView.ViewHolder{
+
+    //private final TextView request_header; // don't need until accept implemented, currently hardcoded string
+    private final TextView requester_name;
+    private final ImageView requester_icon;
+    private final TextView request_text;
+    private final TextView request_item_book_title;
+    FirebaseDatabase mDatabase;
+
+    public RequestHolder(@NonNull View itemView) {
+        super(itemView);
+        request_item_book_title = itemView.findViewById(R.id.request_item_book_title);
+        requester_name = itemView.findViewById(R.id.requester_name);
+        requester_icon = itemView.findViewById(R.id.requester_icon);
+        request_text = itemView.findViewById(R.id.request_text);
+    }
+
+//    public void setRequesterName(String s) {
+//    }
+
+    public void setBookTitle(String bookId) {
+        final String myBookId = bookId;
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference bookRef = mDatabase.getReference("Books");
+
+        ValueEventListener bookListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String bookTitle = dataSnapshot.child(myBookId).child("title").getValue().toString();
+                request_item_book_title.setText(bookTitle);
+                //Log.e("help", requesterName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("cancelled", databaseError.toException());
+            }
+        };
+        bookRef.addListenerForSingleValueEvent(bookListener);
+
+    }
+
+    public void setRequesterIcon(StorageReference s) {
+        Glide.with(itemView.getContext())
+                .load(s).into(requester_icon);
+    }
+
+    public void setRequesterName(String requesterId) {
+        final String reqId = requesterId;
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = mDatabase.getReference("Users");
+
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String requesterName = dataSnapshot.child(reqId).child("username").getValue().toString();
+                requester_name.setText(requesterName);
+                request_text.setText("has requested");
+                Log.e("help", requesterName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("cancelled", databaseError.toException());
+            }
+        };
+        userRef.addListenerForSingleValueEvent(userListener);
+
+    }
+}
---
+++ app/src/main/java/com/example/bookflow/RequestsForBookListActivity.java
@@ -0,0 +1,166 @@
+package com.example.bookflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.bookflow.Model.Notification;
+import com.example.bookflow.Model.Request;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * This class is for the notifications page
+ * received accept or request notifications
+ * are displayed in a recycler view, along
+ * with the senders icon
+ */
+
+public class RequestsForBookListActivity extends BasicActivity {
+    private FirebaseAuth mAuth;
+    private String photoUri;
+    private FirebaseDatabase mDatabase;
+    private FirebaseRecyclerAdapter<Request, RequestHolder> myFirebaseRecyclerAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_notification);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        //String bookId = "-L_dvKIh04uGafLdQzh9";
+        Bundle extras = getIntent().getExtras();
+        final String bookId = extras.getString("bookid");
+
+        DatabaseReference requestsByBookReference = mDatabase.getReference("RequestsReceivedByBook").child(bookId);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setReverseLayout(true);
+        llm.setStackFromEnd(true);
+
+        recyclerView.setLayoutManager(llm);
+
+        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                llm.getOrientation());
+        recyclerView.addItemDecoration(mDividerItemDecoration);
+
+        FirebaseRecyclerOptions<Request> options =
+                new FirebaseRecyclerOptions.Builder<Request>()
+                        .setQuery(requestsByBookReference, Request.class)
+                        .build();
+
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = mDatabase.getReference("Books");
+
+        ValueEventListener bookListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView bookTitleView = findViewById(R.id.notification_header_text);
+                String bookTitle = dataSnapshot.child(bookId).child("title").getValue().toString();
+                bookTitleView.setText("Requests For " + bookTitle);
+                //Log.e("help", requesterName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("cancelled", databaseError.toException());
+            }
+        };
+        userRef.addListenerForSingleValueEvent(bookListener);
+
+        myFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Request, RequestHolder>(options) {
+            @Override
+            public RequestHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.request_list_item, parent, false);
+                return new RequestHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(RequestHolder holder, int position, Request model) {
+                String outString = "";
+
+                String requesterID = model.getBorrowerId();
+                String bookId = model.getBookId();
+                String ownerId = model.getOwnerId();
+                String path = "users/" + requesterID;
+                StorageReference imageRef = FirebaseStorage.getInstance().getReference().child(path);
+
+
+                holder.setRequesterName(requesterID);
+                holder.setRequesterIcon(imageRef);
+                holder.setBookTitle(bookId);
+            }
+        };
+        recyclerView.setAdapter(myFirebaseRecyclerAdapter);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myFirebaseRecyclerAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (myFirebaseRecyclerAdapter!= null) {
+            myFirebaseRecyclerAdapter.stopListening();
+        }
+    }
+
+//    public Request request;
+//    public Request getRequestById(String requestId) {
+//        DatabaseReference requestReference = mDatabase.getReference("Requests").child(requestId);
+//        request = new Request();
+//        ValueEventListener requestListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                request.setBookId(dataSnapshot.child("bookId").getValue().toString());
+//                Log.e("hello", dataSnapshot.child("bookId").getValue().toString());
+//                request.setBorrowerId(dataSnapshot.child("borrowerId").getValue().toString());
+//                request.setStatus(dataSnapshot.child("status").getValue().toString());
+//                request.setOwnerId(dataSnapshot.child("ownerId").getValue().toString());
+//                // must add location when implemented
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                Log.w("cancelled", databaseError.toException());
+//            }
+//        };
+//        requestReference.addValueEventListener(requestListener);
+//        Log.e("hello", request.getBookId());
+//        return request;
+//    }
+}
