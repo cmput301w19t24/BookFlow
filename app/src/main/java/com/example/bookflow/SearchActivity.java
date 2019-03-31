5a8140a74d92931907a8b81aa6d2d65bc7d56ac7
Yuhan Ye <2232191103@qq.com>
2019-03-31 16:57:09 -0600

    search bug fixed

--- app/src/main/java/com/example/bookflow/SearchActivity.java
+++ app/src/main/java/com/example/bookflow/SearchActivity.java
@@ -207,7 +207,7 @@ public class SearchActivity extends BasicActivity {
         @Override
         public void onBindViewHolder(LinearAdapter.LinearViewHolder holder, final int position) {
             holder.setData(mContext,position);
-            if(filtered_books.get(position).getStatus().equals("AVAILABLE") ||filtered_books.get(position).getStatus().equals("REQUESTED")) {
+            if(filtered_books.get(position).getStatus().toString().equals("AVAILABLE") ||filtered_books.get(position).getStatus().toString().equals("REQUESTED")) {
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     //click item to go to book detail page
