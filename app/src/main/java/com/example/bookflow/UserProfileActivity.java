69a5f9b480f2bab014a068652cb70bb6549bfaf8
Yuhan Ye <2232191103@qq.com>
2019-03-31 19:34:19 -0600

    code refactor

--- app/src/main/java/com/example/bookflow/UserProfileActivity.java
+++ app/src/main/java/com/example/bookflow/UserProfileActivity.java
@@ -4,11 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,16 +24,12 @@ import com.bumptech.glide.Glide;
 import com.example.bookflow.Model.Book;
 import com.example.bookflow.Model.Review;
 import com.example.bookflow.Model.User;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -43,7 +37,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 /**
  * This class is for user profile page
@@ -54,20 +47,16 @@ import java.util.UUID;
  */
 public class UserProfileActivity extends BasicActivity {
 
+    private static ListView bookList;
+    private static ListView reviewList;
+    public String uid;
     private FirebaseDatabase database;
     private FirebaseAuth mAuth;
-    public String uid;
-
     private Query query_user;
     private Query query_review;
     private Query query_book;
-
     private ArrayList<Review> reviews;
     private ArrayList<Book> books;
-
-    private static ListView bookList;
-    private static ListView reviewList;
-
     private ReviewAdapter adpReview;
     private BookAdapter adpBook;
 
@@ -75,92 +64,34 @@ public class UserProfileActivity extends BasicActivity {
     private boolean firstIn;
 
     /**
-     * class of review list adapter
-     * it puts comments, reviewer icon, date and rating into an item of the review list
-     */
-    class ReviewAdapter extends ArrayAdapter<Review> {
-        ReviewAdapter(Context c, ArrayList<Review> reviews) {
-            super(c,R.layout.user_list, reviews);
-        }
-
-        @Override
-        public View getView(int position, View v, ViewGroup parent) {
-            Review review = this.getItem(position);
-
-            if (v == null) {
-                v = LayoutInflater.from(getContext()).inflate(R.layout.user_list, parent, false);
-            }
-            // get three views
-            TextView comments = v.findViewById(R.id.review_text);
-            RatingBar rating = v.findViewById(R.id.review_rating);
-            TextView date = v.findViewById(R.id.review_date);
-
-            setReviewUser(v, String.valueOf(review.getReviewerID()));
-
-            // set three views
-            comments.setText(review.getComments());
-            rating.setRating(Float.parseFloat(review.getRating()));
-            date.setText(review.getDate());
-
-            return v;
-        }
-    }
-
-    /**
-     * Class of offered book list, the same as the one in the main page
-     */
-    class BookAdapter extends ArrayAdapter<Book> {
-        BookAdapter(Context c, ArrayList<Book> reviews) {
-            super(c,R.layout.profile_book_item, reviews);
-        }
-
-        @Override
-        public View getView(int position, View v, ViewGroup parent) {
-            Book book = this.getItem(position);
-
-            if (v == null) {
-                v = LayoutInflater.from(getContext()).inflate(R.layout.profile_book_item, parent, false);
-            }
-
-            // get view of author, title, photo, status
-            TextView mauthor = v.findViewById(R.id.uauthor);
-            TextView mtitle = v.findViewById(R.id.utitle);
-            ImageView mphoto = v.findViewById(R.id.uphoto);
-            TextView mstatus = v.findViewById(R.id.ustatus);
-
-            // set author, title, photo, and status
-            mauthor.setText(book.getAuthor());
-            mtitle.setText(book.getTitle());
-            mstatus.setText(book.getStatus().toString());
-            Glide.with(UserProfileActivity.this).load(book.getPhotoUri()).into(mphoto);
-            return v;
-        }
-    }
-
-    /**
      * get the reviewee data from the database and bind them to views
-     * @param v view
+     *
+     * @param v      view
      * @param tmpuid uid
      */
     public void setReviewUser(final View v, final String tmpuid) {
         query_user.orderByChild("uid").equalTo(tmpuid).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                User user = (User) dataSnapshot.getValue(User.class);
+                User user =  dataSnapshot.getValue(User.class);
                 TextView rname = v.findViewById(R.id.reviewer_name);
                 rname.setText(user.getUsername());
                 ImageView mphoto = v.findViewById(R.id.reviewer_photo);
                 Glide.with(UserProfileActivity.this).load(user.getImageurl()).into(mphoto);
             }
+
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
             }
+
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
             }
+
             @Override
             public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
@@ -170,6 +101,7 @@ public class UserProfileActivity extends BasicActivity {
     /**
      * onCreate method
      * also initializes variables
+     *
      * @param savedInstanceState
      */
     @Override
@@ -186,8 +118,8 @@ public class UserProfileActivity extends BasicActivity {
         query_review = database.getReference().child("Reviews");
         query_book = database.getReference().child("Books");
 
-        reviews = new ArrayList<Review>();
-        books = new ArrayList<Book>();
+        reviews = new ArrayList<>();
+        books = new ArrayList<>();
 
         adpReview = new ReviewAdapter(this, reviews);
         adpBook = new BookAdapter(this, books);
@@ -218,7 +150,7 @@ public class UserProfileActivity extends BasicActivity {
             Button writeReview = findViewById(R.id.write_review);
             writeReview.setVisibility(View.VISIBLE);
             uid = message;
-        } else  {
+        } else {
             FirebaseUser user = mAuth.getCurrentUser();
             uid = user.getUid();
         }
@@ -230,7 +162,7 @@ public class UserProfileActivity extends BasicActivity {
     }
 
     private void handleNotif() {
-        FirebaseDatabase database =FirebaseDatabase.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         ValueEventListener notificationListener = new ValueEventListener() {
             @Override
@@ -246,6 +178,7 @@ public class UserProfileActivity extends BasicActivity {
                     notif_count = tmp;
                 }
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.w("cancelled", databaseError.toException());
@@ -284,7 +217,7 @@ public class UserProfileActivity extends BasicActivity {
                 try {
                     storageRef = storage.getReference().child("users").child(uid);
                 } catch (Exception e) {
-                    return ;
+                    return;
                 }
 
                 storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
@@ -296,6 +229,7 @@ public class UserProfileActivity extends BasicActivity {
 
                 });
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
@@ -306,28 +240,34 @@ public class UserProfileActivity extends BasicActivity {
      * load book list from database and add to adapter
      */
     private void loadBookList() {
-        bookList = (ListView) findViewById(R.id.offerList);
+        bookList = findViewById(R.id.offerList);
         // add user's book to adapter
         query_book.orderByChild("ownerId").equalTo(uid).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Book book = (Book)dataSnapshot.getValue(Book.class);
+                Book book =  dataSnapshot.getValue(Book.class);
                 if (!books.contains(book)) {
                     adpBook.add(book);
                 }
             }
+
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 loadBookList();
             }
+
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
                 loadBookList();
             }
+
             @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) { }
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
         });
         bookList.setAdapter(adpBook);
         bookList.setClickable(true);
@@ -346,13 +286,13 @@ public class UserProfileActivity extends BasicActivity {
      * load review list from database and add to adapter
      */
     private void loadReviewList() {
-        reviewList = (ListView) findViewById(R.id.reviewList);
+        reviewList =  findViewById(R.id.reviewList);
         reviews.clear();
         // add user's book to adapter
         query_review.orderByChild("reviewee").equalTo(uid).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Review review = (Review) dataSnapshot.getValue(Review.class);
+                Review review =  dataSnapshot.getValue(Review.class);
                 DataSnapshot reviewerUid = dataSnapshot.child("reviewer");
                 DataSnapshot dateData = dataSnapshot.child("date");
                 if (null != reviewerUid.getValue()) {
@@ -369,17 +309,21 @@ public class UserProfileActivity extends BasicActivity {
                     adpReview.add(review);
                 }
             }
+
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 loadReviewList();
             }
+
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
                 loadReviewList();
             }
+
             @Override
             public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
@@ -389,16 +333,17 @@ public class UserProfileActivity extends BasicActivity {
 
     /**
      * onClick method of Review button
+     *
      * @param view button view
      */
-    public void switchReview(View view){
+    public void switchReview(View view) {
         Button button = findViewById(R.id.review_switch);
         button.setBackgroundResource(R.drawable.tab_select);
-        ((TextView)findViewById(R.id.review_switch)).setTextColor(getResources().getColor(R.color.colorPrimary));
+        ((TextView) findViewById(R.id.review_switch)).setTextColor(getResources().getColor(R.color.colorPrimary));
 
         button = findViewById(R.id.offer_switch);
         button.setBackgroundResource(R.drawable.tab_notselect);
-        ((TextView)findViewById(R.id.offer_switch)).setTextColor(Color.WHITE);
+        ((TextView) findViewById(R.id.offer_switch)).setTextColor(Color.WHITE);
 
         LinearLayout layout = findViewById(R.id.reviewsLayout);
         layout.setVisibility(LinearLayout.VISIBLE);
@@ -409,16 +354,17 @@ public class UserProfileActivity extends BasicActivity {
 
     /**
      * onClick method of Offer button
+     *
      * @param view button view
      */
-    public void switchOffer(View view){
+    public void switchOffer(View view) {
         Button button = findViewById(R.id.review_switch);
         button.setBackgroundResource(R.drawable.tab_notselect);
-        ((TextView)findViewById(R.id.review_switch)).setTextColor(Color.WHITE);
+        ((TextView) findViewById(R.id.review_switch)).setTextColor(Color.WHITE);
 
         button = findViewById(R.id.offer_switch);
         button.setBackgroundResource(R.drawable.tab_select);
-        ((TextView)findViewById(R.id.offer_switch)).setTextColor(getResources().getColor(R.color.colorPrimary));
+        ((TextView) findViewById(R.id.offer_switch)).setTextColor(getResources().getColor(R.color.colorPrimary));
 
         LinearLayout layout = findViewById(R.id.reviewsLayout);
         layout.setVisibility(LinearLayout.GONE);
@@ -429,7 +375,8 @@ public class UserProfileActivity extends BasicActivity {
 
     /**
      * This is onClick method for button "edit profile"
-     * @param view  onClick method needed
+     *
+     * @param view onClick method needed
      */
     public void editProfile(View view) {
         Intent intent = new Intent(this, EditProfileActivity.class);
@@ -439,11 +386,75 @@ public class UserProfileActivity extends BasicActivity {
     /**
      * onClick method of write Review button
      * got to write review activity
-     * @param view
+     *
+     * @param view view
      */
     public void writeReview(View view) {
         Intent intent = new Intent(this, WriteReviewActivity.class);
         intent.putExtra("reviewee", uid);
         startActivity(intent);
+    }
+
+    /**
+     * class of review list adapter
+     * it puts comments, reviewer icon, date and rating into an item of the review list
+     */
+    class ReviewAdapter extends ArrayAdapter<Review> {
+        ReviewAdapter(Context c, ArrayList<Review> reviews) {
+            super(c, R.layout.user_list, reviews);
+        }
+
+        @Override
+        public View getView(int position, View v, ViewGroup parent) {
+            Review review = this.getItem(position);
+
+            if (v == null) {
+                v = LayoutInflater.from(getContext()).inflate(R.layout.user_list, parent, false);
+            }
+            // get three views
+            TextView comments = v.findViewById(R.id.review_text);
+            RatingBar rating = v.findViewById(R.id.review_rating);
+            TextView date = v.findViewById(R.id.review_date);
+
+            setReviewUser(v, String.valueOf(review.getReviewerID()));
+
+            // set three views
+            comments.setText(review.getComments());
+            rating.setRating(Float.parseFloat(review.getRating()));
+            date.setText(review.getDate());
+
+            return v;
+        }
+    }
+
+    /**
+     * Class of offered book list, the same as the one in the main page
+     */
+    class BookAdapter extends ArrayAdapter<Book> {
+        BookAdapter(Context c, ArrayList<Book> reviews) {
+            super(c, R.layout.profile_book_item, reviews);
+        }
+
+        @Override
+        public View getView(int position, View v, ViewGroup parent) {
+            Book book = this.getItem(position);
+
+            if (v == null) {
+                v = LayoutInflater.from(getContext()).inflate(R.layout.profile_book_item, parent, false);
+            }
+
+            // get view of author, title, photo, status
+            TextView mauthor = v.findViewById(R.id.uauthor);
+            TextView mtitle = v.findViewById(R.id.utitle);
+            ImageView mphoto = v.findViewById(R.id.uphoto);
+            TextView mstatus = v.findViewById(R.id.ustatus);
+
+            // set author, title, photo, and status
+            mauthor.setText(book.getAuthor());
+            mtitle.setText(book.getTitle());
+            mstatus.setText(book.getStatus().toString());
+            Glide.with(UserProfileActivity.this).load(book.getPhotoUri()).into(mphoto);
+            return v;
+        }
     }
 }
